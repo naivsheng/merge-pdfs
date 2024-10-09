a443b2cf996cfec43233deab962e636de27749d7
naivsheng <naivsheng@outlook.com>
2024-10-09 13:48:20 +0200

    调整表格列宽，为表格增加目录, 每次运行更新目录页及内页内容

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: naivsheng naivsheng@outlook.com
  * @Date: 2024-01-10 14:21:04
  * @LastEditors: naivsheng naivsheng@outlook.com
- * @LastEditTime: 2024-10-09 12:18:47
+ * @LastEditTime: 2024-10-09 13:47:43
  * @FilePath: \merge-pdfs\README.md
 -->
 # merge-pdfs pdf合并
@@ -26,7 +26,7 @@ merge pdfs and add blank page to single files
 
 用于记录各货柜的文件夹中是否有各店(files.txt)的点货单
 
-调整表格列宽，为表格增加目录
+调整表格列宽，为表格增加目录, 每次运行更新目录页及内页内容
 
 # pdf-split 拆分pdf
 将pdf的各页以Readme.txt文件的页标进行拆分并命名
--- file_Velten.py
+++ file_Velten.py
@@ -1,7 +1,7 @@
 '''
 # -*- coding: UTF-8 -*-
 Author: Yingyu Wang
-LastEditTime: 2024-10-09 12:18:16
+LastEditTime: 2024-10-09 13:37:32
 LastEditors: naivsheng naivsheng@outlook.com
 Description: get当前文件夹内全部文件名以归档: 获取文件夹名并创建对应的sheet页, 将文件夹中的pdf文件汇总到表格中, 调整列宽
 FilePath: \crawer\file_Velten.py
@@ -19,14 +19,14 @@ import openpyxl.worksheet
 path = os.getcwd()
 folder_list = []
 for file in os.listdir(path):
-    if os.path.isdir(path):
+    if os.path.isdir(file):
         folder_list.append(file)
 wb = openpyxl.load_workbook('Velten.xlsx')
-sheets = wb.worksheets
+sheets_name = wb.sheetnames
 for folder in folder_list:
     FLs = read_file()
     pdf_list = {x.replace('\n',''):[] for x in FLs}
-    if folder not in sheets:
+    if folder not in sheets_name:
         ws=wb.create_sheet(folder,0)
         headers = ['Filiale','Files']
         ws.append(headers)
@@ -42,29 +42,32 @@ for folder in folder_list:
             elif filiale not in pdf_list:
                 pdf_list[filiale] = []
             pdf_list[filiale].append(file) 
+    ws.delete_cols(1,ws.max_column)
     for fl,info in pdf_list.items(): # writedown
         row = [fl] + info
         ws.append(row)
     write_file(list(pdf_list.keys()))
     ws.column_dimensions['A'].width = 20
     ws.column_dimensions['B'].width = 50
-sheets = wb.worksheets
-if '目录' not in sheets:
-    ws = wb.create_sheet('目录',0)
-else: ws = wb['目录']
+sheets = wb.sheetnames
+if '目录' in sheets:
+    del wb['目录']
+ws = wb.create_sheet('目录', 0)
 for i, sheet_name in enumerate(sheets):
-    link_address = f"'{sheet_name}'!A1"  # 获取对应的链接地址，此处假设跳转到其他工作表的A1单元格
-    cell = ws.cell(row=i+1, column=1)  # 获取对应的单元格
-    cell.value = f"跳转到 {sheet_name}"
-    cell.hyperlink = openpyxl.worksheet.hyperlink(ref=cell.coordinate, location=link_address)  # 设置跳转链接
-    # 在其他工作表中添加跳转到新增工作表的链接
-    hyperlink_to_new_sheet = openpyxl.worksheet.hyperlink(ref=f"'{ws.title}'!A{cell.row}", location=f"'{ws.title}'!A{cell.row}")
-    # 创建超链接文本
-    text = "返回总表"
-    # 将超链接和文本添加到工作表中
+    if sheet_name == '目录':
+        continue
+    # 在目录工作表中添加跳转到其他工作表的链接
+    cell = ws.cell(row=i + 1, column=1)
+    cell.value = f"Link to {sheet_name}"  # 设置单元格显示文本
+    cell.hyperlink = f"#{sheet_name}!A1"  # 设置跳转链接
+    cell.style = "Hyperlink"  # 设置超链接样式
+    # 在目标工作表中添加返回目录的超链接
     worksheet = wb[sheet_name]
-    max_column = worksheet.max_column
-    return_cell = worksheet.cell(row=1, column=max_column+1)
-    return_cell.value = text
-    return_cell.hyperlink = hyperlink_to_new_sheet
+    return_cell = worksheet.cell(row=1, column=3)
+    # 创建或更新“返回目录”的超链接
+    if return_cell.hyperlink is None or return_cell.hyperlink.target != "目录!A1":
+        return_cell.value = "返回目录"
+        return_cell.hyperlink = f"#目录!A1"  # 设置跳转到新目录页的链接
+        return_cell.style = "Hyperlink"  # 设置超链接样式
+# 保存Excel文件
 wb.save('Velten.xlsx')
